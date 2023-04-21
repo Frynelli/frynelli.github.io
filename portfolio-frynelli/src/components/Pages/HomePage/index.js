7f9800c709361a20c7a963e2afdb2b1a87cbf208
Frynelli Oikonomou <oikonomoufrini@gmail.com>
2023-04-21 22:27:00 +0300

    working on animation and what looks best
    almost finished with the HomePage on desktop, trying to look into details on animation duration etc

--- portfolio-frynelli/src/components/Pages/HomePage/index.js
+++ portfolio-frynelli/src/components/Pages/HomePage/index.js
@@ -1,20 +1,39 @@
+import { useEffect, useState } from "react";
 import selfie from "../../../assets/selfie.jpg";
 
+const typeWebDev = `Your Web Developer`;
 const HomePage =()=>{
+   
+    const [typingWebDev, setTypingWebDev]=useState("");
+    useEffect(()=>{
+        const timeout = setTimeout(()=>{
+         setTypingWebDev(typeWebDev.slice(0, typingWebDev.length + 1))
+        },1000)
+  return ()=> clearTimeout(timeout);
+    },[typingWebDev])
+
     return <div className="container-hp-g">
-        <img src={selfie} alt="selfie"/>
+        
         <div className="welcome-text">
-            <h1>Hi,<br/>I'm Frynelli</h1>
+          
+            <h1>Hi,<br/><span className="name">I'm Frynelli </span><br/><span className="web-dev blinking-cursor"> {typingWebDev}</span></h1>
             <span className="paragraph">
             <p>
-                Junior front end developer & Wordpress    
+                Junior Front End & Wordpress    
             </p>
             <hr/>
             </span>
             
             </div>
-            
-
+            <div className="box-2">
+                <div className="rectangle-purple"></div>
+                <div className="circle-pink">
+            <div className="circle-mask">
+        <img src={selfie} alt="selfie"/>
+        </div>
+                </div>
+            </div>
+        
     </div>
 }
 
