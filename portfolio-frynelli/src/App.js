7a127c3ea16c518ef34c4914f9df480bc4bcfb06
Frynelli Oikonomou <oikonomoufrini@gmail.com>
2023-04-26 22:17:14 +0300

    finished animation letters
    added sample pictures and pdf, trying to figure out how to show a pdf on the website

--- portfolio-frynelli/src/App.js
+++ portfolio-frynelli/src/App.js
@@ -5,6 +5,8 @@ import Layout from './components/Layout';
 import About from "./components/Pages/About";
 import Projects from "./components/Pages/Projects";
 import Contact from "./components/Pages/Contact";
+import Cv from "./components/Pages/About/Cv";
+import friniCv from "./assets/Frynelli-Oikonomou-CV.pdf";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Routes>
        <Route path="/" exact element={<Layout/>}>
         <Route path="/about" element={<About/>}></Route>
+        <Route path="/about/cv" element={<Cv url={friniCv}/>}></Route>
         <Route path="/projects" element={<Projects/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
        </Route>
