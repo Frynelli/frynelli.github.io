f0927fc27d38592ec964b07c729140560aea638b
Frynelli Oikonomou <oikonomoufrini@gmail.com>
2023-04-26 22:25:47 +0300

    created Contact page
    added a link that will go to a form and the back button. Added the images and a div for the mint box

--- portfolio-frynelli/src/components/Pages/Contact/index.js
+++ portfolio-frynelli/src/components/Pages/Contact/index.js
@@ -1,6 +1,50 @@
+import useTypeWriter from "../../TypeWriter/TypeWriter";
+import Form from '../../Form/index'
+import sxedio from '../../../assets/sxedia.png';
+const typeWord = 'Contact Me';
 const Contact =()=>{
-    return <div>
-        This is Contact page
+const ContactMe = useTypeWriter(typeWord)
+    return <div className="container-contact">
+        <div className="box-left">
+            <div className="contact-text">
+                <h1 className="blinking-cursor">{ContactMe}</h1>
+               <div className="background-color">
+               <div className="contact-form">
+               <div className="paragraph">
+                <h3>Let's talk</h3>
+                <p>
+                Wanna get in touch or talk about a project? 
+                  Feel free to <a href="#">contact me</a>
+                </p>
+               </div>
+               
+              
+
+               </div>
+               <div className="button">
+           <button><a href="/">Back</a></button>
+           </div> 
+           </div>
+
+            </div>
+            
+
+        </div>
+        <div className="box-right">
+        
+            <div className="grid-box">
+            <div className="image-sxedio"><img src={sxedio}/></div>
+        <div className="green-box">
+
+        </div>
+            </div>
+            <div className="email-box">
+                <p className="email">email:</p><br/>
+                <p>oikonomoufrini@gmail.com</p>
+               </div>   
+        
+
+        </div>
     </div>
 }
 export default Contact;
