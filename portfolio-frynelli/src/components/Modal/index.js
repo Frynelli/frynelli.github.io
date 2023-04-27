3b0257ecbcdf4b512e7e76cf4365f2b98c3ec123
Frynelli Oikonomou <oikonomoufrini@gmail.com>
2023-04-27 17:20:00 +0300

    Created modal component
    Created modal.js and scss to be able to show my contact form in

---
+++ portfolio-frynelli/src/components/Modal/index.js
@@ -0,0 +1,20 @@
+import {IoCloseSharp} from "react-icons/io5"
+import Form from "../Form";
+
+const MyModal = ({closeModal}) =>{
+  
+  const toggleModal =()=>{
+    closeModal(false);
+  }
+return<>
+<div className="modal">
+<div onClick={toggleModal} className="overlay"></div>
+<div className="modal-content">
+ <Form/>
+<a onClick={toggleModal} className="close-modal"><IoCloseSharp/></a>
+</div>
+</div>
+</>
+}
+
+export default MyModal;
