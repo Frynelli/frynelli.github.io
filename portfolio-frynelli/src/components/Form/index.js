2d2ce5d9d50ef198d18f1ae0552367a7c499eeea
Frynelli Oikonomou <oikonomoufrini@gmail.com>
2023-04-28 11:30:11 +0300

    Added animated letter to form
    centered the form and added animation to contact me

--- portfolio-frynelli/src/components/Form/index.js
+++ portfolio-frynelli/src/components/Form/index.js
@@ -1,20 +1,36 @@
+import { useState, useEffect } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+
 
 const Form = ()=>{
+    const contactArray = ['C','o','n','t','a','c','t','','M','e'];
+    const [letterClass, setLetterClass]=useState("text-animate");
+    useEffect(()=>{
+        const timeout =  setTimeout(()=>{
+            setLetterClass('text-animate-hover')
+        },1500)
+        return ()=> clearTimeout(timeout)
+       },[])
     return<>
         <form className="form-contact">
             <div className="form-title">
-                <h1>Contact Me</h1>
+            
+                <h1><AnimatedLetters 
+                    letterClass={letterClass} 
+                    strArray={contactArray}
+                    idx={18}
+                    /></h1>
                 <p>I am also interested in freelance Wordpress opportunities.</p>
             </div>
             <div className="form-main">
         <label for="name">Name:</label>
-        <input className="input-size" type="text" id="name" name="name" required/>
+        <input className="input-size" activeclassname="active" type="text" id="name" name="name" required/>
         <br/><br/>
         <label for="subject">Subject:</label>
-        <input className="input-size" type="text" id="subject" name="subject" required/>
+        <input className="input-size" activeclassname="active" type="text" id="subject" name="subject" required/>
         <br/><br/>
         <label for="email">Email:</label>
-        <input className="input-size" type="email" id="email" name="email" placeholder="Enter your email" required/>
+        <input className="input-size" activeclassname="active" type="email" id="email" name="email" placeholder="Enter your email" required/>
         <br/><br/>
         <label for="message">Message</label><br/>
         <textarea className="textarea" id="message" name="message" placeholder="Write your message here.." required></textarea>
